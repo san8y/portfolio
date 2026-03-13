2b658231ded684991465dcc875d161259dca84a9
Himanshu <155740533+san8y@users.noreply.github.com>
2026-03-14 00:38:38 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -60,6 +60,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I'm Sandy,a React Developer 
           </p>
 
           <a href="#about">
